1e77acd8ab7b7a658a1a05ef14951b37b6c971a3
NoelWilliam <noelwilliamm@gmail.com>
2024-04-23 11:35:01 +0200

    Importeret stuff

---
+++ src/app/components/Navigation.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default async function Navigation() {
+  return <div></div>;
+}
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,10 @@
 import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
+import { Libre_Baskerville } from "next/font/google";
 import "./globals.css";
+
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +16,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navigation></Navigation>
+        {children}
+        <Footer></Footer>
+      </body>
     </html>
   );
 }
