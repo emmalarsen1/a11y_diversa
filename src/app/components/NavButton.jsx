749a0ddf482c9aa4bce21f741aff5268d6320091
Caroline <cave0001@stud.ked.dk>
2024-04-30 13:41:52 +0200

    knap ally-regler

--- src/app/components/NavButton.jsx
+++ src/app/components/NavButton.jsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 
 function NavButton() {
   return (
-    <div>
-      <Link href={`./regeloversigt`}>Regel-oversigt</Link>
+    <div class="m-4">
+      <Link class=" bg-[#69e3fc] place-items-center hover:bg-[#02E8FE] py-3 px-5  rounded-lg active:bg-[#4BCBE0] text-center font-['Libre_Baskerville'] text-lg font-semibold" href={`./regeloversigt`}>
+        Ally-regler
+      </Link>
     </div>
   );
 }
