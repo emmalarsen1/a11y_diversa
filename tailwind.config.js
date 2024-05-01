ba9966a5a68474f12e72825f73e7b17c45011da0
“Emma <emmalarsen2704@gmail.com>
2024-05-01 12:54:04 +0200

    start styling resultatsiden

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "turquoise-00": "#e6fafe",
+        "orange-70": "#ff7733",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
