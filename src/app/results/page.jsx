020089fb6449ac8304fdaa887706fe6590f84584
NoelWilliam <noelwilliamm@gmail.com>
2024-05-01 13:58:39 +0200

    Databasen virker her

--- src/app/results/page.jsx
+++ src/app/results/page.jsx
@@ -3,13 +3,13 @@ import { dsbData } from "@/data/dsbData";
 import Link from "next/link";
 
 // Revalidate route every 30 minutes
-/* export const revalidate = 1800; */
+export const revalidate = 1800;
 
-export default async function Page() {
-  /*   const params = new URLSearchParams(searchParams);
+export default async function Page({ searchParams }) {
+  const params = new URLSearchParams(searchParams);
   const response = await fetch(`https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`);
-  const data = await response.json(); */
-  const data = dsbData;
+  const data = await response.json();
+  // const data = dsbData;
   const violations = data.violations;
   console.log(violations);
 
