70ffe9d532dbc08e165070d62254b29ae6e77168
NoelWilliam <noelwilliamm@gmail.com>
2024-05-02 14:30:43 +0200

    Justify center tilføjet til regelside

--- src/app/regeloversigt/page.jsx
+++ src/app/regeloversigt/page.jsx
@@ -5,19 +5,13 @@ import { reglerData } from "@/data/reglerData";
 function page() {
   return (
     <>
-      <section class="mx-8 flex flex-col h-lvh">
+      <section class="mx-8 flex justify-center flex-col h-lvh">
         <h1 class="text-5xl m-2.5 mb-8">A11y-regel oversigt</h1>
         <article className="flex flex-col">
           {reglerData.map((data) => (
-            <div
-              class="flex justify-between box-border border-2 bg-turquoise-00 border-transparent border-b-neutral-950 p-4"
-              key={data.id}
-            >
+            <div class="flex justify-between box-border border-2 bg-turquoise-00 border-transparent border-b-neutral-950 p-4" key={data.id}>
               <p class="flex capitalize">{data.id} </p>
-              <Link
-                class="text-[#FF7733] flex underline uppercase"
-                href={`/rules/${data.id}`}
-              >
+              <Link class="text-[#FF7733] flex underline uppercase" href={`/rules/${data.id}`}>
                 Læs mere
               </Link>
             </div>
