bd25a192d5ec25c71765ac93b7c9eb3e0de73951
“Emma <emmalarsen2704@gmail.com>
2024-05-02 12:50:50 +0200

    det virker i det mindste

--- src/app/results/page.jsx
+++ src/app/results/page.jsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import { dsbData } from "@/data/dsbData";
 import Link from "next/link";
+import Gauge from "../components/Gauge";
 
 // Revalidate route every 30 minutes
 /* export const revalidate = 1800; */
 
 const severityMapping = {
+  noerror: {
+    color: "green",
+    value: "0",
+  },
   minor: {
     color: "green",
     value: "25",
@@ -18,7 +23,7 @@ const severityMapping = {
     color: "red",
     value: "75",
   },
-  Critical: {
+  critical: {
     color: "darkred",
     value: "100",
   },
@@ -30,24 +35,65 @@ export default async function Page() {
   const data = await response.json(); */
   const data = dsbData;
   const violations = data.violations;
-  console.log(violations);
 
+  const hasCritical = violations.some(
+    (violation) => violation.impact === "critical"
+  );
+  const hasSerious = violations.some(
+    (violation) => violation.impact === "serious"
+  );
+  const hasModerate = violations.some(
+    (violation) => violation.impact === "moderate"
+  );
+  const hasMinor = violations.some((violation) => violation.impact === "minor");
+
+  let config = {};
+  if (hasCritical) {
+    config = severityMapping.critical;
+  }
+  if (hasSerious) {
+    config = severityMapping.serious;
+  }
+  if (!hasSerious && !hasCritical && !hasModerate && !hasMinor) {
+    config = severityMapping.noerror;
+  }
+  console.log("se", hasMinor);
+  /*
+  loope igennem data
+  finde værste fejl
+  bruge den
+  */
   return (
     <main class=" mx-4 lg:grid grid-cols-2 gap-20 my-20 mx-10">
       <section class="">
-        <h1 class="capitalizetext-base lg:text-xl font-bold">Resultat for {data.url}</h1>
+        <h1 class="capitalizetext-base lg:text-xl font-bold">
+          Resultat for {data.url}
+        </h1>
         <p class="text-base lg:text-xl font-medium">Score:</p>
-        <Image alt={data.url} src={data.screenshot.url} width={data.screenshot.width} height={data.screenshot.height} />
+        <Gauge className="w-1/4" value={config.value} />
+        <Image
+          alt={data.url}
+          src={data.screenshot.url}
+          width={data.screenshot.width}
+          height={data.screenshot.height}
+        />
       </section>
       <section>
-        <p class="text-base lg:text-xl font-bold">Problemer: {data.violations.length}</p>
+        <p class="text-base lg:text-xl font-bold">
+          Problemer: {data.violations.length}
+        </p>
         {violations.map((oneViolation) => {
           return (
             <section key={oneViolation.id} class="gap-y-12">
               <div class="flex gap-3 box-border p-4 border-2 bg-turquoise-00 border-transparent border-b-neutral-950 ">
                 <div>
                   <p class="capitalize">{oneViolation.id}</p>
-                  <p className={`capitalize`} style={{ color: severityMapping[oneViolation.impact].color }}>
+                  <p
+                    className={`capitalize`}
+                    style={{
+                      color: severityMapping[oneViolation.impact].color,
+                    }}
+                  >
                     {oneViolation.impact}
                   </p>
                 </div>
