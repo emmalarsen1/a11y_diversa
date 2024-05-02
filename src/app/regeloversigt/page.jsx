4a04a9e1d7732e19117f10077878626e91529634
NoelWilliam <noelwilliamm@gmail.com>
2024-05-02 10:16:31 +0200

    Fikset konflikt

--- src/app/regeloversigt/page.jsx
+++ src/app/regeloversigt/page.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import Link from "next/link";
 import { reglerData } from "@/data/reglerData";
+
 function page() {
   return (
     <>
       <section class="flex flex-col justify-center items-center h-lvh">
-<<<<<<< HEAD
         <h1 class="text-5xl">A11y-regel oversigt</h1>
-        <article>
-          {reglerData.map((data) => (
-            <p key={data.id}>
-              {data.id} <Link href={`/rules/${data.id}`}>Læs mere</Link>
-=======
-        <h1 class="text-5xl m-2.5 mt-14">A11y-regel oversigt</h1>
         <article class="bg-cyan-200 py-2 px-2 ">
           {reglerData.map((data) => (
             <p class="text-3xl border-b-2 border-black flex place-self-start capitalize" key={data.id}>
@@ -20,7 +14,6 @@ function page() {
               <Link class="text-[#FF7733] underline flex place-self-end uppercase" href={`/rules/${data.id}`}>
                 Læs mere
               </Link>
->>>>>>> noelsbranch
             </p>
           ))}
         </article>
